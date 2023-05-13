6a312c87a028ebf2383ab417a889e1cb75aad264
Shane <killbasab@gmail.com>
2023-05-13 17:51:04 -0400

    fix: remove piece extensions (#25)

--- packages/modules/src/listeners/moduleError.ts
+++ packages/modules/src/listeners/moduleError.ts
@@ -10,7 +10,7 @@ export class ModuleListener extends Listener<typeof ModuleEvents.ModuleError> {
 	}
 
 	public run(error: any, module: Module, _context: ModuleErrorContext) {
-		const { fullName, location } = module;
-		this.container.logger.error(`[Modules Plugin] There was an error in module "${fullName}" at path "${location.full}"\n`, error);
+		const { fullName } = module;
+		this.container.logger.error(`[Modules Plugin] There was an error in module "${fullName}"\n`, error);
 	}
 }
