42495b7504cb2b2e53cfb1f78fd8dfa2645e8e7b
killbasa <killbasab@gmail.com>
2023-12-10 13:22:34 -0500

    fix: optional chaining

--- packages/modules/src/register.ts
+++ packages/modules/src/register.ts
@@ -16,7 +16,7 @@ export class ModulesPlugin extends Plugin {
 	public static [postInitialization](this: SapphireClient): void {
 		const { options, stores } = this;
 
-		if (options.modules?.enabled !== false) {
+		if (options.modules && options.modules.enabled !== false) {
 			stores.get('preconditions').registerPath(join(__dirname, 'preconditions'));
 
 			if (options.modules.loadModuleErrorListeners !== false) {
